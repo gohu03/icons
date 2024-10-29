19cee7bd6efd3cba6b5cfddb3544264eed3aca77
gohu03 <gohu03@gohu.dev>
2024-10-30 04:27:50 +0900

    modified

--- src/create/arrow.rs
+++ src/create/arrow.rs
@@ -1,22 +1,17 @@
-fn round_arrow(width: u8, height: u8, weight: u8, color:&str, degree: u8) -> String{
-  let r:f64 = (weight as f64) / 2.0;
-  let cx:f64 = (width as f64) / 2.0;
-  let angle:f64 = ((((90 - degree) as f64)*std::f64::consts::PI)/180.0).tan();
-  let py:f64 = -angle*(r-cx)+r;
-  
-  let points:[[f64;2];4] = [
-      [cx, r],
-      [cx, (height as f64) - r],
-      [r, py],
-      [(width as f64) - r, py]
-  ];
+pub mod ArrowIcon{
+  pub fn round(width:u8, height:u8, weight:u8, color:&str, degree:u8) -> String{
+    let r:f64 = (weight as f64) / 2.0;
+    let cx:f64 = (width as f64) / 2.0;
+    let angle:f64 = ((((90 - degree) as f64)*std::f64::consts::PI)/180.0).tan();
+    let py:f64 = -angle*(r-cx)+r;
 
-  return format!("<svg width=\"{}\" height=\"{}\" viewBox=\"0 0 {} {}\" xmlns=\"http://www.w3.org/2000/svg\" stroke=\"{}\" stroke-width=\"{}\" stroke-linecap=\"round\"><path d=\"M{} {} V{} M{} {} L{} {} M{} {} L{} {}\"/></svg>", 
-    width, height, width, height, color, weight, 
-    cx, r, 
-    (height as f64) - r, 
-    cx, r, 
-    r, py, 
-    cx, r, 
-    (width as f64) - r, py);
+    return format!("<svg width=\"{}\" height=\"{}\" viewBox=\"0 0 {} {}\" xmlns=\"http://www.w3.org/2000/svg\" stroke=\"{}\" stroke-width=\"{}\" stroke-linecap=\"round\"><path d=\"M{} {} V{} M{} {} L{} {} M{} {} L{} {}\"/></svg>", 
+      width, height, width, height, color, weight, 
+      cx, r, 
+      (height as f64) - r, 
+      cx, r, 
+      r, py, 
+      cx, r, 
+      (width as f64) - r, py);
+  }
 }
--- src/create/menu.rs
+++ src/create/menu.rs
@@ -1,5 +1,5 @@
 pub mod MenuIcon{
-  fn round(width: u8, height: u8, weight: u8, color: &str)->String{
+  pub fn round(width:u8, height:u8, weight:u8, color:&str)->String{
     let rx:f64 = (weight as f64)/2.0;
     return format!("<svg width=\"{}\" height=\"{}\" viewBox=\"0 0 {} {}\" fill=\"{}\" xmlns=\"http://www.w3.org/2000/svg\"><rect x=\"0\" y=\"{}\" width=\"{}\" height=\"{}\" rx=\"{}\"/><rect x=\"0\" y=\"{}\" width=\"{}\" height=\"{}\" rx=\"{}\"/><rect x=\"0\" y=\"{}\" width=\"{}\" height=\"{}\" rx=\"{}\"/></svg>", width, height, width, height, color, 0, width, weight, rx, ((height-weight) as f64)/2.0, width, weight, rx, height-weight, width, weight, rx);
   }
